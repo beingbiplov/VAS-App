48f48786dcf8e10bac12bf0df9e1633addfadaf3
beingbiplov <sharmabiplov@gmail.com>
2022-08-01 20:00:43 +0545

    minor changes

--- src/components/pages/appointmentManagement/AppointmentConfirmation.tsx
+++ src/components/pages/appointmentManagement/AppointmentConfirmation.tsx
@@ -2,24 +2,28 @@ import { Button, Result } from 'antd';
 import React from 'react';
 import { Link } from "react-router-dom";
 
-const AppointmentConfirmation: React.FC = () => (
-  <div className='container formContainer userRegisterConfirmForm'>
-    <Result
-      status="success"
-      title="Successfully Scheduled for Vaccination Appointment!"
-      subTitle="Confirmation Code: S5DSAX "
-      extra={[
-        <Link key="home" to="/">
-          <Button type="primary">
-            Home
-          </Button>
-        </Link>,
-        <Link key="schedule" to="/schedule-appointment">
-          <Button>Schedule Again</Button>,
-        </Link>
-      ]}
-    />
-  </div>
-);
+const AppointmentConfirmation: React.FC = () => {
+  //static confirmation code as confirmation code functionality is not yet implemented
+  const confirmationCode = 'S5DSAX';
+  return(
+    <div className='container formContainer userRegisterConfirmForm'>
+      <Result
+        status="success"
+        title="Successfully Scheduled for Vaccination Appointment!"
+        subTitle={`Confirmation Code: ${confirmationCode}`}
+        extra={[
+          <Link key="home" to="/">
+            <Button type="primary">
+              Home
+            </Button>
+          </Link>,
+          <Link key="schedule" to="/schedule-appointment">
+            <Button>Schedule Again</Button>,
+          </Link>
+        ]}
+      />
+    </div>
+  )
+};
 
 export default AppointmentConfirmation;
