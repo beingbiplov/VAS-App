f9af4c35db6a3791bdf973b68c7408c03e260c9b
beingbiplov <sharmabiplov@gmail.com>
2022-08-05 13:35:48 +0545

    fix typo and format

--- src/components/pages/patientManagement/PatientDetails.tsx
+++ src/components/pages/patientManagement/PatientDetails.tsx
@@ -1,19 +1,23 @@
-import { Modal, Button, Descriptions } from 'antd';
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Modal, Button, Descriptions } from "antd";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import { patientDataInterface } from '../../../redux/interface/patientDataInterface';
+import { patientDataInterface } from "../../../redux/interface/patientDataInterface";
 
-const PatientDetails: React.FC<{patientData:patientDataInterface}> = ({patientData}) => {
+const PatientDetails: React.FC<{ patientData: patientDataInterface }> = ({
+  patientData,
+}) => {
   const [modalVisible, setModalVisible] = useState(false);
 
-  const handleOkBtn =() => {
-    setModalVisible(false)
-  }
+  const handleOkBtn = () => {
+    setModalVisible(false);
+  };
 
   return (
     <div>
-      <Link to='' className='textPrimary' onClick={() => setModalVisible(true)} >View</Link>
+      <Link to="" className="textPrimary" onClick={() => setModalVisible(true)}>
+        View
+      </Link>
       <Modal
         title="Patient Details"
         centered
@@ -21,28 +25,57 @@ const PatientDetails: React.FC<{patientData:patientDataInterface}> = ({patientDa
         visible={modalVisible}
         onCancel={handleOkBtn}
         footer={[
-          <Button className='secondaryOutlineBtn' href={`/patient-upadate/${patientData.email}`} key="update" >
+          <Button
+            className="secondaryOutlineBtn"
+            href={`/patient-update/${patientData.email}`}
+            key="update"
+          >
             Update Patient
           </Button>,
-          <Button key="ok" className='primaryBtn' type="primary" onClick={handleOkBtn}>
+          <Button
+            key="ok"
+            className="primaryBtn"
+            type="primary"
+            onClick={handleOkBtn}
+          >
             Ok
-          </Button>
+          </Button>,
         ]}
       >
         <Descriptions bordered>
-          <Descriptions.Item label="First Name">{patientData.firstName}</Descriptions.Item>
-          <Descriptions.Item label="Last Name">{patientData.lastname}</Descriptions.Item>
-          <Descriptions.Item label="Gender">{patientData.gender}</Descriptions.Item>
-          <Descriptions.Item label="Ethnicity">{patientData.ethnicity}</Descriptions.Item>
+          <Descriptions.Item label="First Name">
+            {patientData.firstName}
+          </Descriptions.Item>
+          <Descriptions.Item label="Last Name">
+            {patientData.lastname}
+          </Descriptions.Item>
+          <Descriptions.Item label="Gender">
+            {patientData.gender}
+          </Descriptions.Item>
+          <Descriptions.Item label="Ethnicity">
+            {patientData.ethnicity}
+          </Descriptions.Item>
           <Descriptions.Item label="DOB" span={2}>
             {patientData.DOB}
           </Descriptions.Item>
-          <Descriptions.Item label="Street">{patientData.address.street}</Descriptions.Item>
-          <Descriptions.Item label="City">{patientData.address.city}</Descriptions.Item>
-          <Descriptions.Item label="Provience">{patientData.address.provience}</Descriptions.Item>
-          <Descriptions.Item label="Insurence ID">{patientData.payment.insurenceId}</Descriptions.Item>
-          <Descriptions.Item label="Member ID">{patientData.payment.memberId}</Descriptions.Item>
-          <Descriptions.Item label="Insurence Provider">{patientData.payment.insurenceProvider}</Descriptions.Item>
+          <Descriptions.Item label="Street">
+            {patientData.address.street}
+          </Descriptions.Item>
+          <Descriptions.Item label="City">
+            {patientData.address.city}
+          </Descriptions.Item>
+          <Descriptions.Item label="Provience">
+            {patientData.address.provience}
+          </Descriptions.Item>
+          <Descriptions.Item label="Insurence ID">
+            {patientData.payment.insurenceId}
+          </Descriptions.Item>
+          <Descriptions.Item label="Member ID">
+            {patientData.payment.memberId}
+          </Descriptions.Item>
+          <Descriptions.Item label="Insurence Provider">
+            {patientData.payment.insurenceProvider}
+          </Descriptions.Item>
         </Descriptions>
       </Modal>
     </div>
--- src/components/pages/patientManagement/PatientList.tsx
+++ src/components/pages/patientManagement/PatientList.tsx
@@ -1,30 +1,30 @@
-import { Button, Input, Space, Table, Typography, InputRef } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
-import type { ColumnsType, ColumnType } from 'antd/es/table';
-import type { FilterConfirmProps } from 'antd/es/table/interface';
-import React, { useRef, useState } from 'react';
-import Highlighter from 'react-highlight-words';
-import { Link } from 'react-router-dom';
+import { Button, Input, Space, Table, Typography, InputRef } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import type { ColumnsType, ColumnType } from "antd/es/table";
+import type { FilterConfirmProps } from "antd/es/table/interface";
+import React, { useRef, useState } from "react";
+import Highlighter from "react-highlight-words";
+import { Link } from "react-router-dom";
 
-import { patientDataInterface } from '../../../redux/interface/patientDataInterface'
-import { getPatientDataLS } from '../../../utils/LocalStorageData';
-import PatientDetails from './PatientDetails';
+import { patientDataInterface } from "../../../redux/interface/patientDataInterface";
+import { getPatientDataLS } from "../../../utils/LocalStorageData";
+import PatientDetails from "./PatientDetails";
 
 const { Title } = Typography;
 
-const PatientList: React.FC = () =>{
-  const [searchText, setSearchText] = useState('');
-  const [searchedColumn, setSearchedColumn] = useState('');
+const PatientList: React.FC = () => {
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
   const searchInput = useRef<InputRef>(null);
 
-  type DataIndex = keyof patientDataInterface
+  type DataIndex = keyof patientDataInterface;
 
-  const data: patientDataInterface[] | undefined = getPatientDataLS()
+  const data: patientDataInterface[] | undefined = getPatientDataLS();
 
   const handleSearch = (
     selectedKeys: string[],
     confirm: (param?: FilterConfirmProps) => void,
-    dataIndex: DataIndex,
+    dataIndex: DataIndex
   ) => {
     confirm();
     setSearchText(selectedKeys[0]);
@@ -33,24 +33,37 @@ const PatientList: React.FC = () =>{
 
   const handleReset = (clearFilters: () => void) => {
     clearFilters();
-    setSearchText('');
+    setSearchText("");
   };
 
-  const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<patientDataInterface> => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+  const getColumnSearchProps = (
+    dataIndex: DataIndex
+  ): ColumnType<patientDataInterface> => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
       <div style={{ padding: 8 }}>
         <Input
           ref={searchInput}
           placeholder={`Search ${dataIndex}`}
           value={selectedKeys[0]}
-          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-          style={{ marginBottom: 8, display: 'block' }}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            handleSearch(selectedKeys as string[], confirm, dataIndex)
+          }
+          style={{ marginBottom: 8, display: "block" }}
         />
         <Space>
           <Button
             type="primary"
-            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+            onClick={() =>
+              handleSearch(selectedKeys as string[], confirm, dataIndex)
+            }
             icon={<SearchOutlined />}
             size="small"
             style={{ width: 90 }}
@@ -79,25 +92,25 @@ const PatientList: React.FC = () =>{
       </div>
     ),
     filterIcon: (filtered: boolean) => (
-      <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+      <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
     ),
     onFilter: (value, record) =>
       record[dataIndex]
         .toString()
         .toLowerCase()
         .includes((value as string).toLowerCase()),
-    onFilterDropdownVisibleChange: visible => {
+    onFilterDropdownVisibleChange: (visible) => {
       if (visible) {
         setTimeout(() => searchInput.current?.select(), 100);
       }
     },
-    render: text =>
+    render: (text) =>
       searchedColumn === dataIndex ? (
         <Highlighter
-          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
           searchWords={[searchText]}
           autoEscape
-          textToHighlight={text ? text.toString() : ''}
+          textToHighlight={text ? text.toString() : ""}
         />
       ) : (
         text
@@ -105,66 +118,81 @@ const PatientList: React.FC = () =>{
   });
 
   const columns: ColumnsType<patientDataInterface> = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    sorter: (a: patientDataInterface, b: patientDataInterface): number =>
-      a.firstName.localeCompare(b.firstName),
-    ...getColumnSearchProps('firstName'),
-    render: (text, record) => (
-      <span>{record.firstName} {record.lastname} </span>
-    )
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-    key: 'email',
-    sorter: (a: patientDataInterface, b: patientDataInterface): number =>
-      a.email.localeCompare(b.email),
-    ...getColumnSearchProps('email')
-  },
-  {
-    title: 'Gender',
-    dataIndex: 'gender',
-    key: 'gender',
-    filters: [
-      { text: 'Male', value: 'Male' },
-      { text: 'Female', value: 'Female' },
-    ],
-    filterMode: 'tree',
-    filterSearch: true,
-    onFilter: (value: any, record):any => record.gender.includes(value),
-  },
-  {
-    title: 'Ethnicity',
-    dataIndex: 'ethnicity',
-    key: 'ethnicity',
-  },
-  {
-    title: 'DOB',
-    dataIndex: 'DOB',
-    key: 'DOB',
-    sorter: (a: patientDataInterface, b: patientDataInterface): number =>
-      a.DOB.localeCompare(b.DOB),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        < PatientDetails patientData={record} />
-        <Link className='textSecondary' to={`/patient-upadate/${record.email}`}>Update</Link> 
-      </Space>
-    ),
-  },
-];
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      sorter: (a: patientDataInterface, b: patientDataInterface): number =>
+        a.firstName.localeCompare(b.firstName),
+      ...getColumnSearchProps("firstName"),
+      render: (text, record) => (
+        <span>
+          {record.firstName} {record.lastname}{" "}
+        </span>
+      ),
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+      sorter: (a: patientDataInterface, b: patientDataInterface): number =>
+        a.email.localeCompare(b.email),
+      ...getColumnSearchProps("email"),
+    },
+    {
+      title: "Gender",
+      dataIndex: "gender",
+      key: "gender",
+      filters: [
+        { text: "Male", value: "Male" },
+        { text: "Female", value: "Female" },
+      ],
+      filterMode: "tree",
+      filterSearch: true,
+      onFilter: (value: any, record): any => record.gender.includes(value),
+    },
+    {
+      title: "Ethnicity",
+      dataIndex: "ethnicity",
+      key: "ethnicity",
+    },
+    {
+      title: "DOB",
+      dataIndex: "DOB",
+      key: "DOB",
+      sorter: (a: patientDataInterface, b: patientDataInterface): number =>
+        a.DOB.localeCompare(b.DOB),
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <PatientDetails patientData={record} />
+          <Link
+            className="textSecondary"
+            to={`/patient-update/${record.email}`}
+          >
+            Update
+          </Link>
+        </Space>
+      ),
+    },
+  ];
 
-  return(
-      <section className='container TableContainer'>
-          <Title className='formContainerHeading' level={4}>Patient List</Title>
-          <Table rowKey={record => record.email} columns={columns} dataSource={data} pagination={{ pageSize:3 }} />;
-      </section>
-  )
-} 
+  return (
+    <section className="container TableContainer">
+      <Title className="formContainerHeading" level={4}>
+        Patient List
+      </Title>
+      <Table
+        rowKey={(record) => record.email}
+        columns={columns}
+        dataSource={data}
+        pagination={{ pageSize: 3 }}
+      />
+      ;
+    </section>
+  );
+};
 export default PatientList;
